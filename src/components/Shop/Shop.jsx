54623ef9bc2c75ef751fa316064b9bf4cf37d5aa
nazirmithu <nazirmithu22@gmail.com>
2023-03-27 23:31:21 +0600

    complete website

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,25 +3,35 @@ import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([]);
-   
-    useEffect (() => {
+    const [cart, setCart] = useState([])
+
+    useEffect(() => {
         fetch('products.json')
-        .then(res => res.json())
-        .then(data => setProducts(data))
+            .then(res => res.json())
+            .then(data => setProducts(data))
     }, [])
+
+    const handleAddToCart = (product) => {
+        // cart.push(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
 
     return (
         <div className='shop-container'>
             <div className="products-container">
-            {
-                products.map(product => <Product
-                key={product.id}
-                product={product}
-                ></Product>)
-            }
+                {
+                    products.map(product => <Product
+                        key={product.id}
+                        product={product}
+                        handleAddToCart={handleAddToCart}
+                    ></Product>)
+                }
             </div>
             <div className="cart-container">
-        <h3>Order Summary</h3>
+                <h3>Order Summary</h3>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
